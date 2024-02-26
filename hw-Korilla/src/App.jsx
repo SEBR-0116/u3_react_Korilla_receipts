9b67556db7f3b60794c8f300d468af4ed6237a3e
maddiemaz <maddiemaz18@gmail.com>
2024-02-26 18:04:04 -0500

    submit

---
+++ hw-Korilla/src/App.jsx
@@ -0,0 +1,15 @@
+import Receipt from './components/Receipt'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Korilla</h1>
+      <div className='receiptDisplay'>
+        <Receipt/>
+      </div>
+    </>
+  )
+}
+export default App
