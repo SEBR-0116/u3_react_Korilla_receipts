77cdf101e3d07b9a9f47835e5a18a472c8dd6b46
rahmed712 <ahmedrahat712@gmail.com>
2024-03-07 20:55:15 -0500

    Hw submit

---
+++ Korilla/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import JeremyCOM from './components/Jeremy'
+import BrittanyCOM from './components/Brittany'
+import TomCOM from './components/Tom'
+
+function App() {
+
+
+  return (
+    <>
+      <JeremyCOM/>
+      <BrittanyCOM/>
+      <TomCOM/>
+    </>
+  )
+}
+
+export default App
